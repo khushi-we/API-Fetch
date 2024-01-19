dd5310aa95955f6abec394f673c8079bd8e13ce5
khushi-we <khushi.c@wheelseye.com>
2024-01-19 12:53:45 +0530

    Initial Commit

---
+++ API Fetch/MyCollectionViewCell.swift
@@ -0,0 +1,15 @@
+//
+//  MyCollectionViewCell.swift
+//  API Fetch
+//
+//  Created by Khushi Chauhan on 19/01/24.
+//
+
+import UIKit
+
+class MyCollectionViewCell: UICollectionViewCell {
+    
+    @IBOutlet var imgview : UIImageView!
+    @IBOutlet var title : UILabel!
+    @IBOutlet var desc : UILabel!
+}
