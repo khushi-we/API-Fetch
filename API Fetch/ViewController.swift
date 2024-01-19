84f3190f71c48102c45b86dd0a5a8418c63d529f
khushi-we <khushi.c@wheelseye.com>
2024-01-19 13:49:55 +0530

    UIView Skeleton Created and requested permissions for audio and video.

--- API Fetch/ViewController.swift
+++ API Fetch/ViewController.swift
@@ -6,12 +6,49 @@
 //
 
 import UIKit
+import AVFoundation
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UICollectionViewDelegate, UICollectionViewDataSource {
+    @IBOutlet var collectionView : UICollectionView!
+    
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return 1
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "cell", for: indexPath) as! MyCollectionViewCell
+        
+        return cell
+    }
+    
+   
+    
 
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        AVCaptureDevice.requestAccess(for: .audio) { granted in
+            if granted
+            {
+                print("Audio Permission Granted")
+            }
+            else
+            {
+                print("Permission not granted")
+            }
+        }
+        AVCaptureDevice.requestAccess(for: .video) { granted in
+            if granted
+            {
+                print("Video Permission Granted")
+            }
+            else
+            {
+                print("Permission not granted")
+            }
+        }
     }
 
 
