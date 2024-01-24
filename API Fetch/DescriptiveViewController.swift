defda28875becb393c3c41a3f2b703dfece4b27b
Khushi Chauhan <khushi.chauhan@com-3-2022-1628.local>
2024-01-24 17:18:39 +0530

    Navigation added to a more descriptive movie page when a movie cell is tapped

---
+++ API Fetch/DescriptiveViewController.swift
@@ -0,0 +1,29 @@
+//
+//  DescriptiveViewController.swift
+//  API Fetch
+//
+//  Created by Khushi Chauhan on 24/01/24.
+//
+
+import UIKit
+
+class DescriptiveViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
